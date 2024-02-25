6337c7095382092e7f795269027bfa43637e9bb8
전성훈 <shajh9708@naver.com>
2024-02-25 18:52:30 +0900

    chore: initial commit

---
+++ src/test/java/com/engtalkmo/EngtalkmoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engtalkmo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngtalkmoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
