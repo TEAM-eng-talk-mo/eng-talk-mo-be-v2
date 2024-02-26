821130c22db8b4e75f7cb42931d8a8d3b0152425
전성훈 <shajh9708@naver.com>
2024-02-26 23:58:07 +0900

    feat: spring security config

---
+++ src/main/java/com/engtalkmo/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.engtalkmo.config;
+
+import com.engtalkmo.domain.user.UserDetailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@Configuration
+//@EnableWebSecurity
+public class SecurityConfig {
+
+    private final UserDetailService userDetailService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/login", "/sign-up", "/user").permitAll()
+                        .requestMatchers("/", "/css/**", "/images/**", "/js/**", "/favicon.ico", "/h2-console/**").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(configurer -> configurer
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/"))
+                .logout(configurer -> configurer
+                        .logoutSuccessUrl("/login")
+                        .invalidateHttpSession(true))
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(BCryptPasswordEncoder passwordEncoder,
+                                                       UserDetailService userDetailService) {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailService);
+        provider.setPasswordEncoder(passwordEncoder);
+        return new ProviderManager(provider);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
